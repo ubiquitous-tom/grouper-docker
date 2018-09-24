9d25c9243e4f32d932b2b9d91f003ff7a14164cf
Tom koston <omnipresent_tom@hotmail.com>
2018-09-24 00:22:14 -0700

    added more layout for the login dashboard

--- src/grouper/app/Http/Controllers/MemberController.php
+++ src/grouper/app/Http/Controllers/MemberController.php
@@ -13,7 +13,7 @@ class MemberController extends Controller
      */
     public function index()
     {
-        return view('member.index');
+        return view('members.index');
     }
 
     /**
